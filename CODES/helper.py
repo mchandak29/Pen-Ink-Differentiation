94a1a436fa919e25c92eac9bf9a4e06c3f731992
Mohit Chandak <mchandak29@gmail.com>
2018-07-24 13:45:56 +0530

    Classifier

---
+++ CODES/helper.py
@@ -0,0 +1,273 @@
+from matplotlib import pyplot as plt
+import math
+from keras.callbacks import LambdaCallback
+import keras.backend as K
+from keras.callbacks import *
+import pandas as pd
+
+class LRFinder:
+    """
+    Plots the change of the loss function of a Keras model when the learning rate is exponentially increasing.
+    See for details:
+    https://towardsdatascience.com/estimating-optimal-learning-rate-for-a-deep-neural-network-ce32f2556ce0
+    """
+    def __init__(self, model):
+        self.model = model
+        self.losses = []
+        self.lrs = []
+        self.best_loss = 1e9
+
+    def on_batch_end(self, batch, logs):
+        # Log the learning rate
+        lr = K.get_value(self.model.optimizer.lr)
+        self.lrs.append(lr)
+
+        # Log the loss
+        loss = logs['loss']
+        self.losses.append(loss)
+
+        # Check whether the loss got too large or NaN
+        if math.isnan(loss) or loss > self.best_loss * 10:
+            self.model.stop_training = True
+            return
+
+        if loss < self.best_loss:
+            self.best_loss = loss
+
+        # Increase the learning rate for the next batch
+        lr *= self.lr_mult
+        K.set_value(self.model.optimizer.lr, lr)
+
+    def find(self, x_train, y_train, start_lr, end_lr, batch_size=64, epochs=1):
+        num_batches = epochs * x_train.shape[0] / batch_size
+        self.lr_mult = (end_lr / start_lr) ** (1 / num_batches)
+
+        # Save weights into a file
+        self.model.save_weights('tmp.h5')
+
+        # Remember the original learning rate
+        original_lr = K.get_value(self.model.optimizer.lr)
+
+        # Set the initial learning rate
+        K.set_value(self.model.optimizer.lr, start_lr)
+
+        callback = LambdaCallback(on_batch_end=lambda batch, logs: self.on_batch_end(batch, logs))
+
+        self.model.fit(x_train, y_train,
+                        batch_size=batch_size, epochs=epochs,
+                        callbacks=[callback])
+
+        # Restore the weights to the state before model fitting
+        self.model.load_weights('tmp.h5')
+
+        # Restore the original learning rate
+        K.set_value(self.model.optimizer.lr, original_lr)
+
+    def plot_loss(self, n_skip_beginning=10, n_skip_end=5):
+        """
+        Plots the loss.
+        Parameters:
+            n_skip_beginning - number of batches to skip on the left.
+            n_skip_end - number of batches to skip on the right.
+        """
+        plt.ylabel("loss")
+        plt.xlabel("learning rate (log scale)")
+        plt.plot(self.lrs[n_skip_beginning:-n_skip_end], self.losses[n_skip_beginning:-n_skip_end])
+        plt.xscale('log')
+
+    def plot_loss_change(self, sma=1, n_skip_beginning=10, n_skip_end=5, y_lim=(-0.01, 0.01)):
+        """
+        Plots rate of change of the loss function.
+        Parameters:
+            sma - number of batches for simple moving average to smooth out the curve.
+            n_skip_beginning - number of batches to skip on the left.
+            n_skip_end - number of batches to skip on the right.
+            y_lim - limits for the y axis.
+        """
+        assert sma >= 1
+        derivatives = [0] * sma
+        for i in range(sma, len(self.lrs)):
+            derivative = (self.losses[i] - self.losses[i - sma]) / sma
+            derivatives.append(derivative)
+
+        plt.ylabel("rate of loss change")
+        plt.xlabel("learning rate (log scale)")
+        plt.plot(self.lrs[n_skip_beginning:-n_skip_end], derivatives[n_skip_beginning:-n_skip_end])
+        plt.xscale('log')
+        plt.ylim(y_lim)
+
+
+
+class CyclicLR(Callback):
+    """This callback implements a cyclical learning rate policy (CLR).
+    The method cycles the learning rate between two boundaries with
+    some constant frequency, as detailed in this paper (https://arxiv.org/abs/1506.01186).
+    The amplitude of the cycle can be scaled on a per-iteration or 
+    per-cycle basis.
+    This class has three built-in policies, as put forth in the paper.
+    "triangular":
+        A basic triangular cycle w/ no amplitude scaling.
+    "triangular2":
+        A basic triangular cycle that scales initial amplitude by half each cycle.
+    "exp_range":
+        A cycle that scales initial amplitude by gamma**(cycle iterations) at each 
+        cycle iteration.
+    For more detail, please see paper.
+    
+    # Example
+        ```python
+            clr = CyclicLR(base_lr=0.001, max_lr=0.006,
+                                step_size=2000., mode='triangular')
+            model.fit(X_train, Y_train, callbacks=[clr])
+        ```
+    
+    Class also supports custom scaling functions:
+        ```python
+            clr_fn = lambda x: 0.5*(1+np.sin(x*np.pi/2.))
+            clr = CyclicLR(base_lr=0.001, max_lr=0.006,
+                                step_size=2000., scale_fn=clr_fn,
+                                scale_mode='cycle')
+            model.fit(X_train, Y_train, callbacks=[clr])
+        ```    
+    # Arguments
+        base_lr: initial learning rate which is the
+            lower boundary in the cycle.
+        max_lr: upper boundary in the cycle. Functionally,
+            it defines the cycle amplitude (max_lr - base_lr).
+            The lr at any cycle is the sum of base_lr
+            and some scaling of the amplitude; therefore 
+            max_lr may not actually be reached depending on
+            scaling function.
+        step_size: number of training iterations per
+            half cycle. Authors suggest setting step_size
+            2-8 x training iterations in epoch.
+        mode: one of {triangular, triangular2, exp_range}.
+            Default 'triangular'.
+            Values correspond to policies detailed above.
+            If scale_fn is not None, this argument is ignored.
+        gamma: constant in 'exp_range' scaling function:
+            gamma**(cycle iterations)
+        scale_fn: Custom scaling policy defined by a single
+            argument lambda function, where 
+            0 <= scale_fn(x) <= 1 for all x >= 0.
+            mode paramater is ignored 
+        scale_mode: {'cycle', 'iterations'}.
+            Defines whether scale_fn is evaluated on 
+            cycle number or cycle iterations (training
+            iterations since start of cycle). Default is 'cycle'.
+    """
+
+    def __init__(self, base_lr=0.001, max_lr=0.006, step_size=2000., mode='triangular',
+                 gamma=1., scale_fn=None, scale_mode='cycle'):
+        super(CyclicLR, self).__init__()
+
+        self.base_lr = base_lr
+        self.max_lr = max_lr
+        self.step_size = step_size
+        self.mode = mode
+        self.gamma = gamma
+        if scale_fn == None:
+            if self.mode == 'triangular':
+                self.scale_fn = lambda x: 1.
+                self.scale_mode = 'cycle'
+            elif self.mode == 'triangular2':
+                self.scale_fn = lambda x: 1/(2.**(x-1))
+                self.scale_mode = 'cycle'
+            elif self.mode == 'exp_range':
+                self.scale_fn = lambda x: gamma**(x)
+                self.scale_mode = 'iterations'
+        else:
+            self.scale_fn = scale_fn
+            self.scale_mode = scale_mode
+        self.clr_iterations = 0.
+        self.trn_iterations = 0.
+        self.history = {}
+
+        self._reset()
+
+    def _reset(self, new_base_lr=None, new_max_lr=None,
+               new_step_size=None):
+        """Resets cycle iterations.
+        Optional boundary/step size adjustment.
+        """
+        if new_base_lr != None:
+            self.base_lr = new_base_lr
+        if new_max_lr != None:
+            self.max_lr = new_max_lr
+        if new_step_size != None:
+            self.step_size = new_step_size
+        self.clr_iterations = 0.
+        
+    def clr(self):
+        cycle = np.floor(1+self.clr_iterations/(2*self.step_size))
+        x = np.abs(self.clr_iterations/self.step_size - 2*cycle + 1)
+        if self.scale_mode == 'cycle':
+            return self.base_lr + (self.max_lr-self.base_lr)*np.maximum(0, (1-x))*self.scale_fn(cycle)
+        else:
+            return self.base_lr + (self.max_lr-self.base_lr)*np.maximum(0, (1-x))*self.scale_fn(self.clr_iterations)
+        
+    def on_train_begin(self, logs={}):
+        logs = logs or {}
+
+        if self.clr_iterations == 0:
+            K.set_value(self.model.optimizer.lr, self.base_lr)
+        else:
+            K.set_value(self.model.optimizer.lr, self.clr())        
+            
+    def on_batch_end(self, epoch, logs=None):
+        
+        logs = logs or {}
+        self.trn_iterations += 1
+        self.clr_iterations += 1
+
+        self.history.setdefault('lr', []).append(K.get_value(self.model.optimizer.lr))
+        self.history.setdefault('iterations', []).append(self.trn_iterations)
+
+        for k, v in logs.items():
+            self.history.setdefault(k, []).append(v)
+        
+        K.set_value(self.model.optimizer.lr, self.clr())
+
+
+
+def get_penmap():
+    file_name = '/home/mohit/Desktop/dataset/DATA.xlsx'
+    fn = '/home/mohit/Desktop/dataset/Mapping Cheque Numbers.xlsx'
+
+    xl_file = pd.ExcelFile(file_name)
+
+    dfs = {sheet_name: xl_file.parse(sheet_name) 
+              for sheet_name in xl_file.sheet_names}
+
+    xl = pd.ExcelFile(fn)
+    df = {sheet_name: xl.parse(sheet_name) 
+              for sheet_name in xl.sheet_names}
+
+    s1 = df['Sheet1']['Cheque Num.']
+    s2 = df['Sheet1']['Original Cheque Numbers']
+    s3 = dfs['cheque data']['CHEQUE NO ']
+    s4 = dfs['cheque data']['PEN1']
+    s5 = dfs['cheque data']['PEN 2']
+    mapp={}
+    for i in range(1,113):
+        c = 'Cheque'+str(i)
+        idx = s1[s1==c].index[0]
+        mapp[int(s2[idx][6:len(s2[idx])])] = s1[idx]
+
+    dicc = {}
+    for i in range(0,len(s3)):
+        try:
+            dicc[mapp[s3[i]]] = [s4[i],s5[i]]
+        except:
+            continue
+    a = {}
+    for i in range(1,15):
+        a[i] = []
+        for keys in dicc.keys():
+            if(dicc[keys][0] ==i or dicc[keys][1]==i):
+                a[i].append(int(keys[6:len(keys)]))
+    
+    return a
+
+
+#just a comment
